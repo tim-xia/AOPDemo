25f1f100267eda58313948afea9008667396d78c
Tian-Ming Xia <xiatm@mwclg.net>
2018-05-18 15:34:01 +0800

    AOP编程以及Wrapper模式实现

---
+++ AOPDemo/DecoratorDemo.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOPDemo
+{
+    /// <summary>
+    /// 装饰者Demo类
+    /// </summary>
+    public class DecoratorDemo
+    {
+        /// <summary>
+        /// Component
+        /// </summary>
+        public interface IBeverage
+        {
+            string GetDescription();
+            double GetPrice();
+        }
+
+        /// <summary>
+        /// ConcreteComponent
+        /// </summary>
+        public class CoffeeBean1 : IBeverage
+        {
+            private string description = "第一种咖啡豆";
+            public string GetDescription()
+            {
+                return description;
+            }
+
+            public double GetPrice()
+            {
+                return 50;
+            }
+        }
+
+        /// <summary>
+        /// ConcreteComponent
+        /// </summary>
+        public class CoffeeBean2 : IBeverage
+        {
+            private string description = "第二种咖啡豆";
+            public string GetDescription()
+            {
+                return description;
+            }
+
+            public double GetPrice()
+            {
+                return 55;
+            }
+        }
+
+        /// <summary>
+        /// Decorator
+        /// </summary>
+        public class Decorator : IBeverage
+        {
+            protected  IBeverage _beverage;
+
+            protected Decorator(IBeverage beverage)
+            {
+                this._beverage = beverage;
+            }
+
+            public  string GetDescription()
+            {
+               return  _beverage.GetDescription();
+            }
+
+            public  double GetPrice()
+            {
+               return _beverage.GetPrice();
+            }
+        }
+
+        /// <summary>
+        /// DecoratorConcrete
+        /// </summary>
+        public class Milk : Decorator
+        {
+            private string description = "加牛奶";
+
+            public Milk(IBeverage beverage) : base(beverage)
+            {
+            }
+
+            public new  string GetDescription()
+            {
+               return _beverage.GetDescription() + description;
+            }
+
+            public new double GetPrice()
+            {
+                return _beverage.GetPrice() + 8;
+            }
+
+        }
+
+        /// <summary>
+        /// DecoratorConcrete
+        /// </summary>
+        public class Mocha : Decorator
+        {
+            private string description = "加摩卡";
+            public Mocha(IBeverage beverage) : base(beverage)
+            {
+            }
+            public new string GetDescription()
+            {
+                return _beverage.GetDescription() + description;
+            }
+
+            public new double GetPrice()
+            {
+                return _beverage.GetPrice() + 8;
+            }
+        }
+    }
+}
---
+++ AOPDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AOPDemo.DecoratorDemo;
+using static System.Console;
+
+namespace AOPDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //装饰者模式的简单实用
+            //IBeverage coffeebean1=new CoffeeBean1();
+            //IBeverage conffeebean2=new CoffeeBean2();
+            //Milk milk=new Milk(coffeebean1);
+            //Mocha mocha=new Mocha(conffeebean2);
+            //WriteLine(milk.GetDescription());
+            //WriteLine(milk.GetPrice());
+            //WriteLine(mocha.GetDescription());
+            //WriteLine(mocha.GetPrice());
+
+            ReadLine();
+        }
+    }
+}
